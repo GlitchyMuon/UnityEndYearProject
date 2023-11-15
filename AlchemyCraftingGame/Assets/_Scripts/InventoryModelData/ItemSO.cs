9c3fc9e67f165983239d168c66c0880900933125
GlitchyMuon <glitchy-muon@proton.me>
2023-11-15 16:43:12 +0100

    Added UI sprites and an Inventory grid, scrollview, scrollbar. Added InventorySO and some yet not functioning code and scripts like InventoryParameterSO and a line in ItemSO for InventoryParameter.

--- AlchemyCraftingGame/Assets/_Scripts/InventoryModelData/ItemSO.cs
+++ AlchemyCraftingGame/Assets/_Scripts/InventoryModelData/ItemSO.cs
@@ -2,39 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Ingredient Data", menuName = "ScriptableObjects/Item SO")]   //will create an asset menu option from ItemSO inside the CreateAsset Menu
-public class ItemSO : ScriptableObject
+namespace Inventory.Model
 {
-    [field : SerializeField]
+    [CreateAssetMenu(fileName = "Ingredient Data", menuName = "ScriptableObjects/Item SO")]   //will create an asset menu option from ItemSO inside the CreateAsset Menu
+    public class ItemSO : ScriptableObject
+    {
+        [field : SerializeField]
 
-    public bool IsStackable { get; set; }
+        public bool IsStackable { get; set; }
 
-    public int ID => GetInstanceID();   //will return a unique instance ID for each instanciated scirptable object item. So we can check and compare if we have an instance of "" object in our inventory by detecting if we have an object with this ID already in our inventory
+        public int ID => GetInstanceID();   //will return a unique instance ID for each instanciated scirptable object item. So we can check and compare if we have an instance of "" object in our inventory by detecting if we have an object with this ID already in our inventory
 
-    [field : SerializeField]
-    public int MaxStackSize { get; set; } = 1;
+        [field : SerializeField]
+        public int MaxStackSize { get; set; } = 1;
 
-    [field : SerializeField]
-    public string Name { get; set; }
+        [field : SerializeField]
+        public string Name { get; set; }
 
-    /// <summary>
-    /// Spefificies an item type. String type.
-    /// </summary>
-    /// <value>Herb, Flower, Gemstone or Creature</value>
-    [field : SerializeField]
-    public string ItemType { get; set; }
+        /// <summary>
+        /// Spefificies an item type. String type.
+        /// </summary>
+        /// <value>Herb, Flower, Gemstone or Creature</value>
+        [field : SerializeField]
+        public string ItemType { get; set; }
 
-    /// <summary>
-    /// Elemental type of the item
-    /// </summary>
-    /// <value>Moon, Sun, Time, Earth, Wind, Electricity, Fire or Water</value>
-    [field : SerializeField]
-    public string ElementalType { get; set; }
-    
-    [field : SerializeField]
-    [field : TextArea]
-    public string Description { get; set; }
+        /// <summary>
+        /// Elemental type of the item
+        /// </summary>
+        /// <value>Moon, Sun, Time, Earth, Wind, Electricity, Fire or Water</value>
+        [field : SerializeField]
+        public string ElementalType { get; set; }
+        
+        [field : SerializeField]
+        [field : TextArea]
+        public string Description { get; set; }
 
-    [field : SerializeField]
-    public Sprite ItemImage { get; set; }
+        [field : SerializeField]
+        public Sprite ItemImage { get; set; }
+
+        [field : SerializeField]
+        // public List<ItemParameter> DefaultParameters { get; set; }
+    }
+
+    [Serializable]
+    public struct ItemParameter : IEquatable<ItemParameter>
+    {
+        public ItemParameterSO itemParameter;
+        public float value;
+
+        public bool Equals(ItemParameter other)
+        {
+            return other.itemParameter == itemParameter;
+        }
+    }
 }
