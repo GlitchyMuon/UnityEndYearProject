d01245862696c4e0408a0a040dd36690d1709ade
GlitchyMuon <glitchy-muon@proton.me>
2023-11-30 16:36:11 +0100

    Added a scrollbar UI to the RecipeList UI. Added a recipe prefab. Created a RecipeListLinkToSO.cs. Moved a line to Start() in MagicCircleSlotContentManager.cs

--- AlchemyCraftingGame/Assets/_Scripts/MagicCircleSlotContentManager.cs
+++ AlchemyCraftingGame/Assets/_Scripts/MagicCircleSlotContentManager.cs
@@ -6,12 +6,11 @@ public class MagicCircleSlotContentManager : MonoBehaviour
 {
     RecipeSO activeRecipe;
 
-    DraggableItem[] ingredients = FindObjectsOfType<DraggableItem>();
-
     public ItemSO.ElementalType element;    //need to point out the class where the enum is!
     // Start is called before the first frame update
     void Start()
     {
+        DraggableItem[] ingredients = FindObjectsOfType<DraggableItem>();
         
     }
 
---
+++ AlchemyCraftingGame/Assets/_Scripts/RecipeListLinkToSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeListLinkToSO : MonoBehaviour
+{
+    GridLayoutGroup gridLayoutGroup;
+    public RecipeSO myRecipeSO;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
